67b6ac296f23fa02b34d9ee661e50bbed5324707
luist23 <00127717@uca.edu.sv>
2019-03-19 19:49:11 -0600

    finish?

--- app/src/main/java/com/example/myapplication02/MainActivity.java
+++ app/src/main/java/com/example/myapplication02/MainActivity.java
@@ -5,11 +5,30 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Integer[] blue;
+    private Integer[] green;
+    private Integer[] red;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        blue = new Integer[]{R.color.color3,R.color.color31,R.color.color32};
+        green = new Integer[]{R.color.color1,R.color.color11,R.color.color12};
+        red = new Integer[]{R.color.color2,R.color.color21,R.color.color22};
+
+        findViewById(R.id.color1).setOnClickListener(v->{
+            int ram = (int)(Math.random()*2);
+            v.setBackgroundColor(green[ram]);
+        });
+        findViewById(R.id.color2).setOnClickListener(v->{
+            int ram = (int)(Math.random()*2);
+            v.setBackgroundColor(red[ram]);
+        });
+        findViewById(R.id.color3).setOnClickListener(v->{
+            int ram = (int)(Math.random()*2);
+            v.setBackgroundColor(blue[ram]);
+        });
 
     }
 }
